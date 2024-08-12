997b5ea5248998b9f27c02634da61af6047a36fa
ming <185431334@qq.com>
2024-08-12 09:23:12 +0800

    新增游戏得分

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Game from 'pages/index.jsx';
 
+// 全局对象，用于调试信息输出
+Object.defineProperty(window, 'DEBUG_INFO', {
+  get: function () {
+    const githubStr = `项目地址：https://github.com/PiDazhong/flappybird`;
+    const gameStr = `部署地址：https://flappybird.quantanalysis.cn`;
+
+    return `\n${githubStr}\n\n${gameStr}\n`;
+  },
+});
+
+console.log(window['DEBUG_INFO']);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Game />
